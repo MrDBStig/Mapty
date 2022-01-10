38f32c40835367f381345b927aafeb2782ef4892
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-01-10 11:37:13 +0300

    Implemented Leaflet library

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ if (navigator.geolocation) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(latitude, longitude);
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert(`Could not get your position.`);
