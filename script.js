beb4ffdae6bfd4b0c2d4aea1b210feeff63fa084
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-01-10 11:26:47 +0300

    Implemented Geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+    },
+    function () {
+      alert(`Could not get your position.`);
+    }
+  );
+}
